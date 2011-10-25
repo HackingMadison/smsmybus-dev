17fe83d296644d2036096edbaf1ac6cf9b321a29
Greg Tracy <gtracy@gmail.com>
2011-10-24 23:04:17 -0500

    adding new examples. the app/ directory contains the implementations for the SMS, chat, email and phone interfaces

---
+++ apps/config.py
@@ -0,0 +1,14 @@
+
+ACCOUNT_SID   = "fixme"
+ACCOUNT_TOKEN = "fixme"
+API_VERSION   = '2010-04-01'
+CALLER_ID     = '+1AAABBBCCCC'
+
+ABUSERS = []
+
+METRO_API_KEY = 'fixme'
+
+URL_BASE = 'http://smsmybus.appspot.com/'
+
+EMAIL_SENDER_ADDRESS = 'fixme@co.com'
+EMAIL_BCC_ADDRESS    ='fixme@email.com'
